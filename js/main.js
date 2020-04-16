a9baf364920283f21bda3ee22e865abee0bc5855
eip <eip@users.noreply.github.com>
2020-04-17 02:42:03 +0300

    Initialize color picker state

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
-/* global lch2sRGB, sRGBfloat2int */
+/* global lch2sRGB, sRGBfloat2int, sRGBhex2lch */
 
 'use strict';
 
 const state = {
   colorSpace: {
     dimension: {
-      h: { name: 'hue', min: 0, max: 360, step: 0.1 },
-      c: { name: 'chroma', min: 0, max: 135, step: 0.1 },
-      l: { name: 'lightness', min: 0, max: 100, step: 0.1 }
-    },
-    axis: [
-      ['hlc', 'hue-lightness'],
-      ['clh', 'chroma-lightness'],
-      ['hcl', 'hue-chroma']
-    ]
+      l: { name: 'lightness', index: 0, min: 0, max: 100, step: 0.1 },
+      c: { name: 'chroma', index: 1, min: 0, max: 135, step: 0.1 },
+      h: { name: 'hue', index: 2, min: 0, max: 360, step: 0.5 }
+    }
   },
-  zval: 42.32,
+  axes: 'hcl',
+  from: '#c76584',
+  to: '#30c2f8',
   busy: false
 };
 
@@ -29,6 +26,17 @@ const sliderStepFine = sliderStep / 10;
 const sliderAxisLabel = select('#slider-axis');
 const sliderValueLabel = select('#slider-value');
 const isFirefox = navigator.userAgent.toLowerCase().includes('firefox');
+
+function is(type, value) {
+  return (
+    ![undefined, null].includes(value) &&
+    (typeof type === 'string' ? value.constructor.name : value.constructor) === type
+  );
+}
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 function select(selector, first) {
   if (first === undefined) first = selector.startsWith('#'); // eslint-disable-line no-param-reassign
@@ -109,7 +117,7 @@ function renderColorSpace() {
   const maxChVal = 1.000005;
 
   if (state.scale) {
-    doRenderColorSpace();
+    doRender();
     return;
   }
   state.scale = 0;
@@ -117,11 +125,11 @@ function renderColorSpace() {
     state.scale++;
     colorSpace.width = colorSpace.height = canvasSize / state.scale; // eslint-disable-line no-multi-assign
     const start = performance.now();
-    doRenderColorSpace();
+    doRender();
     if (performance.now() - start < 100) break;
   }
 
-  function doRenderColorSpace() {
+  function doRender() {
     const {
       h: { min: xmin, max: xmax },
       l: { min: ymin, max: ymax }
@@ -165,21 +173,24 @@ function renderColorSpace() {
   }
 }
 
-slider.addEventListener('input', () => {
-  state.zval = parseFloat(slider.value);
-  sliderValueLabel.innerText = slider.value;
-  if (state.busy) return;
-  state.busy = true;
-  setTimeout(() => {
-    const t0 = performance.now();
-    renderColorSpace();
-    state.busy = false;
-    const t1 = performance.now();
-    console.log(`renderColorSpace() took ${t1 - t0} milliseconds.`);
-  }, 1);
-});
+function updateAxes(axes) {
+  state.axes = axes;
+  [state.ax, state.ay, state.az] = axes;
+  const zdim = state.colorSpace.dimension[state.az];
+  sliderAxisLabel.innerText = capitalize(zdim.name);
+  slider.step = zdim.step / 1000;
+  slider.value = state.from[zdim.index].toFixed(Math.round(-Math.log10(slider.step)));
+  slider.dispatchEvent(new InputEvent('input'));
+  slider.step = zdim.step;
+}
 
-window.addEventListener('load', () => slider.dispatchEvent(new InputEvent('input')));
+function init() {
+  state.from = is(String, state.from) ? sRGBhex2lch(state.from) : state.from;
+  state.to = is(String, state.to) ? sRGBhex2lch(state.to) : state.to;
+  updateAxes(state.axes);
+}
+
+window.addEventListener('load', init);
 
 document.addEventListener('keydown', e => {
   if (slider.step !== sliderStep) return;
@@ -205,6 +216,20 @@ slider.addEventListener('keydown', e => {
   if (['ArrowLeft', 'ArrowDown'].includes(e.code)) slider.stepDown();
   else slider.stepUp();
   slider.dispatchEvent(new InputEvent('input'));
+});
+
+slider.addEventListener('input', () => {
+  state.zval = parseFloat(slider.value);
+  sliderValueLabel.innerText = slider.value;
+  if (state.busy) return;
+  state.busy = true;
+  setTimeout(() => {
+    const t0 = performance.now();
+    renderColorSpace();
+    state.busy = false;
+    const t1 = performance.now();
+    console.log(`renderColorSpace() took ${t1 - t0} milliseconds.`);
+  }, 1);
 });
 
 select('.tab[data-view]').forEach(el => el.addEventListener('click', () => switchView(el)));
